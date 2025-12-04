ad37bb0fece4f8f1a74c0050faa40ba8598e2369
Danny <Dannylin2006@gmail.com>
2025-12-03 20:42:34 -0500

    added add

---
+++ userTyping.m
@@ -0,0 +1,24 @@
+% Danny Lin
+function isCorrect = userTyping(word, gameScene, img, windowWidth, windowHeight) %figure out a way to not count things like []\;,. without hardcoding a bunch of conditoinal
+     userInput = '';
+     fprintf(word);
+     key = getKeyboardInput(gameScene);
+        while ~strcmp(key,'return')
+        if strcmp(key, 'backspace')
+            if ~isempty(userInput)
+                userInput = userInput(1:end-1);
+            end
+        elseif length(key) == 1 && isstrprop(key, "alpha")
+            userInput = [userInput char(key)];
+        else            
+        end
+        img = drawWord(img, windowWidth, windowHeight, userInput);
+        drawScene(gameScene, img);
+        key = getKeyboardInput(gameScene);
+        end 
+        if strcmp(userInput, word)
+            isCorrect = true;
+        else 
+            isCorrect = false;
+        end
+end
